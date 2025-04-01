e8f138cc5838530eee3ebfb837482a930ee1251a
PedroCR5 <123810586+PedroCR5@users.noreply.github.com>
2025-04-02 00:17:16 +0200

    MorePixel

--- pixel.js
+++ pixel.js
@@ -12,32 +12,32 @@ export function pixelImg(imgUsadaCard, iCard) {
 
   const allPixels = [];
   console.log(allPixels);
-
-  img.onload = function () {
-    // Ajustar el tamaño del canvas al tamaño de la imagen
-    canvas.width = img.width
-    canvas.height = img.height
-    // Dibujar la imagen en el canvas
-    ctx.drawImage(canvas, 1, 0);
-    // Obtener los datos de los píxeles
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    console.log(imageData);
-
-    const data = imageData.data;
-    // Recorrer cada píxel
-    console.log(`Red: ${data[0]}, Green: ${data[1]}, Blue: ${data[2]}, Alpha: ${data[3]}`);
-    for (let i = 0; i < data.length; i += 4) {
-      const red = data[i];
-      const green = data[i + 1];
-      const blue = data[i + 2];
-      const alpha = data[i + 3];
-      var colorPixel = `${red}, ${green}, ${blue}`;
-      allPixels.push(colorPixel);
-      //console.log(allPixels);
-
-    }
-  }
-  img.onload();
+  /* 
+    img.onload = function () {
+      // Ajustar el tamaño del canvas al tamaño de la imagen
+      canvas.width = img.width
+      canvas.height = img.height
+      // Dibujar la imagen en el canvas
+      ctx.drawImage(canvas, 1, 0);
+      // Obtener los datos de los píxeles
+      const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+      console.log(imageData);
+  
+      const data = imageData.data;
+      // Recorrer cada píxel
+      console.log(`Red: ${data[0]}, Green: ${data[1]}, Blue: ${data[2]}, Alpha: ${data[3]}`);
+      for (let i = 0; i < data.length; i += 4) {
+        const red = data[i];
+        const green = data[i + 1];
+        const blue = data[i + 2];
+        const alpha = data[i + 3];
+        var colorPixel = `${red}, ${green}, ${blue}`;
+        allPixels.push(colorPixel);
+        //console.log(allPixels);
+  
+      }
+    } */
+  // img.onload();
   function encontrarValorMasRepetido(arr) {
     const contador = {};
     let maxRepeticiones = 0;
